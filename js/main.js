d91808476093d20ada1aa6f354a3ca012993f6e8
jisun22 <hayanangela@gmail.com>
2021-10-22 19:02:49 +0900

    madehome

--- js/main.js
+++ js/main.js
@@ -14,3 +14,10 @@ var swiper = new Swiper('.swiper-container', {
     },
     speed: 1000
   });
+
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
